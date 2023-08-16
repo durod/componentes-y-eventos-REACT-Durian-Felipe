bc6f9f2f12cdc05aa572507d722b404a54cf256a
Eduardo <durian277@gmail.com>
2023-08-15 22:29:24 -0600

    se crean los inputs correo y contraseña

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,18 +1,38 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 
-const Formulario = ({ onSubmit, nombre, correo, contraseña, onChange, errores }) => {
+const Formulario = ({
+  onSubmit,
+  nombre,
+  correo,
+  contraseña,
+  onChange,
+  errores,
+}) => {
   return (
     <form onSubmit={onSubmit}>
       <div>
-        <label>Nombre:</label>
-        <input
-          type="text"
-          name="nombre"
-          value={nombre}
-          onChange={onChange}
-        />
+        <label>Nombre: </label>
+        <input type="text" name="nombre" value={nombre} onChange={onChange} />
         {errores.nombre && <span className="error">{errores.nombre}</span>}
+
+        
+          <label>Correo: </label>
+          <input
+            type="email"
+            name="correo"
+            value={correo}
+            onChange={onchange}
+          />
+        
+
+        <label>Contraseña: </label>
+        <input
+          type="password"
+          name="contraseña"
+          value={contraseña}
+          onChange={onchange}
+        />
       </div>
       {/* Resto de los campos y validaciones */}
       <button type="submit">Registrar</button>
